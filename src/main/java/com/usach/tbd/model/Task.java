8799a8250e85967e1ccb75e0386ac309c2bb4bfe
matias <matias.pizarro@usach.cl>
2019-09-02 15:48:15 -0400

    Fix
    Fix

--- src/main/java/com/usach/tbd/model/Task.java
+++ src/main/java/com/usach/tbd/model/Task.java
@@ -45,4 +45,9 @@ public class Task {
             inverseJoinColumns = @JoinColumn(name = "characteristic_id", referencedColumnName = "id"))
     private Set<Characteristic> characteristics;
 
+
+    public Long getId() {
+        return id;
+    }
+
 }
