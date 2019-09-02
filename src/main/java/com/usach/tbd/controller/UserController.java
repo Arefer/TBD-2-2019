a017909df36df1f2aa5aeea8277295e42f0b08d4
Kevin Arévalo <kevin.arevalo@usach.cl>
2019-09-02 18:11:47 -0400

    user CRUD

---
+++ src/main/java/com/usach/tbd/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.usach.tbd.controller;
+
+import com.usach.tbd.model.User;
+import com.usach.tbd.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> listUsers() {
+        return (List<User>) this.userRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public User createUser(@RequestBody User user) {
+        return this.userRepository.save(user);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public User getUser(@PathVariable Long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        if (user.getId() != Integer.parseInt(id)){
+            return null;
+        }
+        return this.userRepository.save(user);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public User deleteUser(@PathVariable Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        user.ifPresent(value -> this.userRepository.delete(value));
+        return null;
+    }
+}
