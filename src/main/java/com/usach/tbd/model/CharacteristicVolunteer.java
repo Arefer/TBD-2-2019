07c31cc75d0911bbe6377957183c0ac5e8aac06d
Kevin Arévalo <kevin.arevalo@usach.cl>
2019-09-17 19:41:56 -0300

    Created model for characteristic_volunteer intermediate table

---
+++ src/main/java/com/usach/tbd/model/CharacteristicVolunteer.java
@@ -0,0 +1,66 @@
+package com.usach.tbd.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "characteristic_volunteer")
+public class CharacteristicVolunteer {
+    @EmbeddedId
+    private CharacteristicVolunteerId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("characteristicId")
+    private Characteristic characteristic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("volunteerId")
+    private Volunteer volunteer;
+
+    public CharacteristicVolunteer() { }
+
+    public CharacteristicVolunteer(Characteristic c, Volunteer v){
+        this.characteristic = c;
+        this.volunteer = v;
+        this.id = new CharacteristicVolunteerId(c.getId(), v.getId());
+    }
+
+    public CharacteristicVolunteerId getId() {
+        return id;
+    }
+
+    public void setId(CharacteristicVolunteerId id) {
+        this.id = id;
+    }
+
+    public Characteristic getCharacteristic() {
+        return characteristic;
+    }
+
+    public void setCharacteristic(Characteristic characteristic) {
+        this.characteristic = characteristic;
+    }
+
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CharacteristicVolunteer)) return false;
+        CharacteristicVolunteer that = (CharacteristicVolunteer) o;
+        return getId().equals(that.getId()) &&
+                getCharacteristic().equals(that.getCharacteristic()) &&
+                getVolunteer().equals(that.getVolunteer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCharacteristic(), getVolunteer());
+    }
+}
