9a71bd81f1dd4b697d6ad7073474dae29334a467
matias <matias.pizarro@usach.cl>
2019-09-22 21:42:04 -0300

    cors again!

--- src/main/java/com/usach/tbd/controller/CharacteristicController.java
+++ src/main/java/com/usach/tbd/controller/CharacteristicController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/characteristics")
 public class CharacteristicController {
     private CharacteristicRepository characteristicRepository;
--- src/main/java/com/usach/tbd/controller/TaskController.java
+++ src/main/java/com/usach/tbd/controller/TaskController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/tasks")
 public class TaskController {
     private TaskRepository taskRepository;
